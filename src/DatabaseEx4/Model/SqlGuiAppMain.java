41cad1b0638923385249801593e24faad60e75c5
benwex93 <benwex93@hotmail.com>
2016-06-15 15:46:31 +0300

    Connected database to sql

---
+++ src/DatabaseEx4/Model/SqlGuiAppMain.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DatabaseEx4.Model;
+
+import DatabaseEx4.View.SqlGui;
+
+/**
+ *
+ * @author LPAS004587
+ */
+public class SqlGuiAppMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        SqlGui NJF = new SqlGui();
+        NJF.show();
+    }
+    
+}
